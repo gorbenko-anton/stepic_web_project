7b7787cc9e867138b77708f4402577e38dcf0125
Gorbenko Anton Vasilevich <AGorbenko@gksm.local>
2020-09-10 10:07:01 +0300

    push it

--- hello.py
+++ hello.py
@@ -1,20 +1,5 @@
 from django.http import HttpResponse
 
-
-
 def return_params(request):
     body = [bytes(i + '\n', 'ascii') for i in environ['QUERY_STRING'].split('&')]
     return HttpResponse(body)
-
-
-
-    def app(environ, start_response):
-    """Simplest possible application object"""
-    data = b'Hello, World!\n'
-    status = '200 OK'
-    response_headers = [
-        ('Content-type', 'text/plain'),
-        ('Content-Length', str(len(data)))
-    ]
-    start_response(status, response_headers)
-    return iter([data])
--- init.sh
+++ init.sh
@@ -2,4 +2,6 @@ sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
 sudo rm -rf /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
 cd /home/box/web
-gunicorn --bind='0.0.0.0:8080' hello:return_params
+#gunicorn --bind='0.0.0.0:8080' hello:return_params
+ln -s /home/box/web/etc/gunicorn.conf /etc/gunicorn.d/test
+sudo service gunicorn restart
