fe874fc3745a358ec5e603ffabf7c617e55242c1
Gorbenko Anton Vasilevich <AGorbenko@gksm.local>
2020-09-11 09:07:38 +0300

    push it

--- init.sh
+++ init.sh
@@ -2,6 +2,6 @@ sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
 sudo rm -rf /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
 cd /home/box/web
-#gunicorn --bind='0.0.0.0:8080' hello:return_params
-sudo ln -s /home/box/web/etc/gunicorn.conf /etc/gunicorn.d/test
-sudo service gunicorn restart
+sudo gunicorn --bind='0.0.0.0:8080' hello:return_params
+#sudo ln -s /home/box/web/etc/gunicorn.conf /etc/gunicorn.d/test
+#sudo service gunicorn restart
