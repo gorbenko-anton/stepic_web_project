4efbc162b38cc2668005b406b370c44b7777f294
Gorbenko Anton Vasilevich <AGorbenko@gksm.local>
2020-09-11 09:18:39 +0300

    push it

--- hello.py
+++ hello.py
@@ -1,5 +1,5 @@
 from django.http import HttpResponse
 
-def return_params(request):
+def return_params(environ, request):
     body = [bytes(i + '\n', 'ascii') for i in environ['QUERY_STRING'].split('&')]
     return HttpResponse(body)
