504150aa94f9c9957f534557b96ac57df2b28f0e
Gorbenko Anton Vasilevich <AGorbenko@gksm.local>
2020-09-15 08:25:00 +0300

    push it

--- hello.py
+++ hello.py
@@ -1,12 +1,7 @@
 from django.http import HttpResponse
 
 def return_params(environ, start_response):
-    data = [bytes('\r\n'.join(environ['QUERY_STRING'].split('&')), encoding="utf8")]
-    status = '200 OK'
-    response_headers = [
-        ('Content-type', 'text/plain'),
-        ('Content-Length', str(len(data)))
-    ]
-    start_response(status, response_headers)
-    print(data)
-    return data
+    start_response('200 OK', [('Content-Type', 'text/plain')])
+    print([bytes('\r\n'.join(environ['QUERY_STRING'].split('&')), 'ascii')])
+    return [bytes('\r\n'.join(environ['QUERY_STRING'].split('&')), 'ascii')]
+    
