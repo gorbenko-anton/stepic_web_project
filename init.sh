776513542f4eed29cfa21188ce5e0e6df77c2924
Gorbenko Anton Vasilevich <AGorbenko@gksm.local>
2020-09-19 13:41:29 +0300

    push it

--- init.sh
+++ init.sh
@@ -3,5 +3,7 @@ sudo rm -rf /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
 cd /home/box/web
 sudo ln -s /home/box/web/etc/gunicorn.conf /etc/gunicorn.d/test
-sudo gunicorn --bind='0.0.0.0:8080' hello:return_params
-#sudo service gunicorn restart
+sudo gunicorn --bind='0.0.0.0:8080' hello:return_params &
+sudo django-admin.py startproject ask .
+sudo ./manage.py startapp qa
+cp ./uploads/views.py ./ask/qa/
