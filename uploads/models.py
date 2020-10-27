4ed351b65d49e87d1ec49e3318e7f266d7be1cf2
Gorbenko Anton Vasilevich <AGorbenko@gksm.local>
2020-10-27 09:21:23 +0300

    push it

--- uploads/models.py
+++ uploads/models.py
@@ -7,6 +7,7 @@ from django.db import models
 from django.contrib.auth.models import User
 
 class Question(models.Model):
+  objects = QuestionManager()
   title = models.CharField(max_length=255)
   text = models.TextField()
   added_at = models.DateTimeField(auto_now_add=True)
