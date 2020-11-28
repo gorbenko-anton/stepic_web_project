40401d2bb2286968867bbe1f9ebfd58c8c8fcae4
Gorbenko Anton Vasilevich <AGorbenko@gksm.local>
2020-11-28 18:49:34 +0300

    push it

--- uploads/views.py
+++ uploads/views.py
@@ -21,7 +21,7 @@ def post_details(request, id):
     })
 
 def new_questions(request):
-    questions = get_object_or_404(Question, id=1)#Question.objects.new() #.filter(is_published=True)
+    questions = Answer.objects.new() #.filter(is_published=True)
     limit = request.GET.get('limit', 10)
     page = request.GET.get('page', 1)
     paginator = Paginator(questions, limit)
