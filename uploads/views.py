41f35464d12d9da0663134d7383fba2e69a731b4
Gorbenko Anton Vasilevich <AGorbenko@gksm.local>
2020-11-29 09:48:27 +0300

    push it

--- uploads/views.py
+++ uploads/views.py
@@ -4,7 +4,7 @@ from django.shortcuts import render, get_object_or_404
 from django.core.urlresolvers import reverse
 from django.core.paginator import Paginator
 
-#from qa import models
+from qa import models
 
 
 #from ask.qa.models import Question
