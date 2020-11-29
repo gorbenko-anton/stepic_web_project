d74954a79fbc09969099e034988ef5d69a52a369
Gorbenko Anton Vasilevich <AGorbenko@gksm.local>
2020-11-29 09:55:32 +0300

    push it

--- uploads/views.py
+++ uploads/views.py
@@ -10,7 +10,7 @@ from qa import models
 #from ask.qa.models import Question
 #from qa import Question
 #from ask import models
-from ask.qa import models
+#from ask.qa import models
 #from ask import Question
 
 def test(request, *args, **kwargs):
